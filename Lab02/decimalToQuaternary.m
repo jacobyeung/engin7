edb6357b9f78927281dd3219c1165ead3a1d0c18
Jacob Yeung <jacobyeung@berkeley.edu>
2020-02-02 02:51:33 -0800

    Fixed lab2 p3

--- Lab02/decimalToQuaternary.m
+++ Lab02/decimalToQuaternary.m
@@ -1,42 +1,11 @@
 function quaternary = decimalToQuaternary(decimal)
-sum = 0;
-power = 0;
-solution = recurse(decimal);
-    function answer = recurse(dec)    
-    while dec ~= 0
-        digit = dec - 10 * floor(dec / 10);
-        if digit < 4
-            sum = sum + digit * 10^power;
-        elseif digit == 4
-            sum = sum + 1 * 10^(power + 1);
-        elseif digit == 5
-            sum = sum + 1 * 10^(power + 1) + 1 * 10^(power);
-        elseif digit == 6
-            sum = sum + 1 * 10^(power + 1) + 2 * 10^(power);
-        elseif digit == 7
-            sum = sum + 1 * 10^(power + 1) + 3 * 10^(power);
-        elseif digit == 8
-            sum = sum + 2 * 10^(power + 1);
-        elseif digit == 9
-            sum = sum + 2 * 10^(power + 1) + 1 * 10^(power);
-        end
-        power = power + 1;
-        dec = floor(dec / 10);
-    end
-    answer = sum;
-    end
-resum = recurse(sum);
-if resum == sum
-    arr = [0, 0, 0, 0, 0, 0, 0, 0];
-    for i = 1:8
-        digit = solution - 10 * floor(solution / 10);
-        solution = floor(solution / 10);
-        arr(9 - i) = digit;
-    end
-    quaternary = arr;
-
-else
-    quaternary = decimalToQuaternary(sum);
+sum = [0, 0, 0, 0, 0, 0, 0, 0];
+count = 8;
+while decimal > 0
+    remainder = mod(decimal, 4);
+    sum(count) = remainder;
+    decimal = (decimal - remainder) / 4;
+    count = count - 1;
 end
-
+quaternary = sum;
 end
