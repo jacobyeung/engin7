1eb2d7c727359411ac5c02453a76b93ecdd0fd21
Jacob Yeung <jacobyeung@berkeley.edu>
2020-02-29 16:10:31 -0800

    Back to finish comparePrecision

--- Lab05/cheeseBoard.m
+++ Lab05/cheeseBoard.m
@@ -2,12 +2,16 @@ function path = cheeseBoard(current, prev_path)
 d = [current(1, 1) + 1, current(1, 2)];
 r = [current(1, 1), current(1, 2) + 1];
 persistent p;
+persistent board;
+if isempty(board)
+    board = char.empty;
+end
 if eq(size(prev_path), [0 0])
     p = 1;
     prev_path = char.empty;
 end
 if eq(current, [5 5])
-    path(p, 1:length(prev_path)) = prev_path;
+    board(p, 1:length(prev_path)) = prev_path;
     p = p + 1;
 elseif ~myCheck(d) || ~myCheck(r)
     return;
@@ -16,18 +20,19 @@ else
         if size(prev_path) == 0
             prev_path = 'R';
         else
-            prev_path = strcat(prev_path, 'R');
+            prev_path = [prev_path, 'R'];
         end
-        path = cheeseBoard(r, prev_path);
+        board = cheeseBoard(r, prev_path);
     end
     if myCheck(d)
         if size(prev_path) == 0
             prev_path = 'D';
         else
-            prev_path = strcat(prev_path, 'D');
+            prev_path = [prev_path, 'D'];
         end
-        path = cheeseBoard(d, prev_path);
+        board = cheeseBoard(d, prev_path);
     end
-    return;
+end
+path = board;
 end
 
